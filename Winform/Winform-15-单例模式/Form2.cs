cbfdf876d0b49ee78e711a24c4ef96c744add1bb
shanzm <2351144690@qq.com>
2018-06-05 20:25:26 +0800

    005

---
+++ Winform/Winform-15-单例模式/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Winform_15_单例模式
+{
+    public partial class Form2 : Form
+    {
+        ///全局唯一单例（就是一个字段，为GetForm中新建Form时作限制
+        ///为什么是静态的呢？因为GetForm函数是静态的，只能调用静态成员
+        public static Form2 Form2Single;
+
+      private  Form2()//首先将构造函数改为私有
+        {
+            InitializeComponent();
+        }
+
+
+        public static Form2 GetForm()
+        {
+            if (Form2Single==null)
+            {
+                Form2Single = new Form2();
+            }
+           return Form2Single;
+        }
+    }
+}
