3e6d85b5fb4b29e200452a9a31911530467df207
shanzm_aimu <2351144690@qq.com>
2021-07-28 16:06:16 +0800

    2021年7月28日 16:05:57-Add:winform控件库

---
+++ Winform控件库/MsgBox/FmTester.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace AhDung.WinForm
+{
+    public partial class FmTester : Form
+    {
+        MessageBoxButtons msgButtons;
+        MessageBoxIcon msgIcon;
+        MessageBoxDefaultButton msgDfButton;
+
+        public FmTester()
+        {
+            InitializeComponent();
+
+            grpMsbButtons.Tag = typeof(MessageBoxButtons);
+            grpMsgIcon.Tag = typeof(MessageBoxIcon);
+            grpDfButton.Tag = typeof(MessageBoxDefaultButton);
+
+            msgButtons = MessageBoxButtons.OK;
+            msgIcon = MessageBoxIcon.None;
+            msgDfButton = MessageBoxDefaultButton.Button1;
+        }
+
+        private void btnShowMsgEx_Click(object sender, EventArgs e)
+        {
+            txbResult.AppendText(
+            MsgBox.Show(txbMessage.Text
+                , txbCaption.Text
+                , txbAttachMessage.Text
+                , msgButtons
+                , msgIcon
+                , msgDfButton
+                , ckbExpand.Checked
+                , txbButtonsText.Lines
+                )
+                + "\r\n");
+        }
+
+        private void btnShowMsgStd_Click(object sender, EventArgs e)
+        {
+            txbResult.AppendText(
+            MessageBox.Show(txbMessage.Text
+                , txbCaption.Text
+                , msgButtons
+                , msgIcon
+                , msgDfButton
+                )
+                + "\r\n");
+        }
+
+        private void RadioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (!rb.Checked) { return; }
+
+            Type t = rb.Parent.Tag as Type;
+            object val = Enum.Parse(t, rb.Tag?.ToString() ?? rb.Text);
+            if (t == typeof(MessageBoxButtons)) { msgButtons = (MessageBoxButtons)val; }
+            else if (t == typeof(MessageBoxIcon)) { msgIcon = (MessageBoxIcon)val; }
+            else { msgDfButton = (MessageBoxDefaultButton)val; }
+        }
+
+        private void ckbEnableAnimate_CheckedChanged(object sender, EventArgs e)
+        {
+            MsgBox.EnableAnimate = ckbEnableAnimate.Checked;
+        }
+
+        private void ckbEnableSound_CheckedChanged(object sender, EventArgs e)
+        {
+            MsgBox.EnableSound = ckbEnableSound.Checked;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.cnblogs.com/ahdung/p/4631233.html");
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            MsgBox.ShowInfo(txbMessage.Text, txbAttachMessage.Text, txbCaption.Text, ckbExpand.Checked, GetFirstLine(txbButtonsText));
+        }
+
+        private void btnShowWarning_Click(object sender, EventArgs e)
+        {
+            MsgBox.ShowWarning(txbMessage.Text, txbAttachMessage.Text, txbCaption.Text, ckbExpand.Checked, GetFirstLine(txbButtonsText));
+        }
+
+        private void btnShowError_Click(object sender, EventArgs e)
+        {
+            MsgBox.ShowError(txbMessage.Text, txbAttachMessage.Text, txbCaption.Text, ckbExpand.Checked, GetFirstLine(txbButtonsText));
+        }
+
+        private void btnShowQuestion_Click(object sender, EventArgs e)
+        {
+            var result = MsgBox.ShowQuestion(txbMessage.Text, txbAttachMessage.Text, defaultButton: msgDfButton, expand: ckbExpand.Checked, buttonsText: txbButtonsText.Lines);
+            txbResult.AppendText($"{result}\r\n");
+        }
+
+        static string GetFirstLine(TextBox txb) => txb.Text.Substring(0, txb.Text.IndexOf('\r') + 1);
+    }
+}
---
+++ Winform控件库/MsgBox/TestFrom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AhDung.WinForm
+{
+    public partial class TestFrom : Form
+    {
+        public TestFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //txbResult.AppendText(
+           //MessageBox.Show(txbMessage.Text
+           //    , txbCaption.Text
+           //    , msgButtons
+           //    , msgIcon
+           //    , msgDfButton
+           //    )
+               //+ "\r\n");
+        }
+    }
+}
