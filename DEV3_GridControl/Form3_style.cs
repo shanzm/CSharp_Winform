56d95926b65202902b77edc820eaaa1f17418700
shanzm_aimu <2351144690@qq.com>
2021-09-09 13:48:14 +0800

    2021年9月9日 13:47:56-Update：关于GridControl样式的一些设置

---
+++ DEV3_GridControl/Form3_style.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEV3_GridControl
+{
+    public partial class Form3_style : Form
+    {
+        public Form3_style()
+        {
+            InitializeComponent();
+        }
+
+        ///Editable=False设置为不可编辑
+        ///ColumnAutoWidth=False设置关闭列宽自动
+        ///ShowGroupPanel=False设置为不限分组栏
+    }
+}
