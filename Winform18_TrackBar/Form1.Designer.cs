57a9ed18bf7e2c0f6a43d2c80c4c4ec9b52970f9
shanzm_aimu <2351144690@qq.com>
2021-09-15 13:15:08 +0800

    2021年9月15日 13:14:30-Add：关于TrackBar滑动控件的使用demo

---
+++ Winform18_TrackBar/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Winform18_TrackBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labTrackBarValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(176, 52);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(280, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TrackBar控件";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 201);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "拖动选中的值";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labTrackBarValue);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(534, 136);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "滑块控件";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "拖动选中的值（100:1）";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 310);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(270, 25);
+            this.textBox2.TabIndex = 6;
+            // 
+            // labTrackBarValue
+            // 
+            this.labTrackBarValue.AutoSize = true;
+            this.labTrackBarValue.Location = new System.Drawing.Point(462, 62);
+            this.labTrackBarValue.Name = "labTrackBarValue";
+            this.labTrackBarValue.Size = new System.Drawing.Size(15, 15);
+            this.labTrackBarValue.TabIndex = 2;
+            this.labTrackBarValue.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labTrackBarValue;
+    }
+}
+
---
+++ Winform18_TrackBar/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Winform18_TrackBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTrackBarControl();
+        }
+        //滚动事件
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //拖动滑块，在label中显示value
+            labTrackBarValue.Text = trackBar1.Value.ToString();
+
+            textBox1.Text = trackBar1.Value.ToString();
+            //注意：TrackBar的value是int类型，所以若是需要设置精度为小数则，可以按照比率来，比如TrackBar值与真实值1：0.1
+            textBox2.Text = ((double)trackBar1.Value / 100).ToString();
+        }
+
+        //选中值发生变化的事件
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void InitTrackBarControl()
+        {
+            this.trackBar1.Minimum = 0;
+            this.trackBar1.Maximum = 100;
+        }
+    }
+}
