56226edf6e966da78c1a156f550ba2d9d9d49bdd
shanzm_aimu <2351144690@qq.com>
2021-09-15 10:56:27 +0800

    2021年9月15日 10:56:06-Update：关于EventLog系统日志控件的使用

---
+++ Winform17-EventLog控件/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Winform17_EventLog控件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(353, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(353, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "写入事件日志";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 181);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(353, 257);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(353, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "读取日志";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
+
---
+++ Winform17-EventLog控件/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Winform17_EventLog控件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ///注意EventLog是在 System.Diagnostics命名空间下的
+        ///
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+
+            //注意我们需要使用管理员身份运行程序（vs需要使用管理员身份运行）
+            //否则报错：未找到源，但未能搜索某些或全部事件日志。  不可访问的日志: Security。
+
+            //查看事件日志：
+            //控件面板-->所有控制面板想-->管理工具-->事件查看器-->应用程序和服务日志-->找到我们自定义的日志：newevnetlog
+
+            if (EventLog.SourceExists("logsource"))
+            {
+                EventLog.DeleteEventSource("logsource");
+            }
+            EventLog.CreateEventSource("logsource", "neweventlog");//第一个参数：应用程序在本地注册时使用的源名称，第二个参数：日志名称
+            eventLog1.Log = "neweventlog";
+            eventLog1.Source = "logsource";
+            eventLog1.MachineName = ".";//表示为本地机上
+
+        }
+        //写入日志按钮点击事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (EventLog.Exists("neweventlog"))
+            {
+                if (textBox1.Text != "")
+                {
+                    eventLog1.WriteEntry(textBox1.Text);
+                    MessageBox.Show("写入日志成功", "提示");
+                    textBox1.Text = "";
+                    this.button2_Click(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("日志内容不能为空", "提示");
+                }
+            }
+            else
+            {
+                MessageBox.Show("日志不存在", "提示");
+            }
+        }
+
+        //读取日志按钮点击事件
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (eventLog1.Entries.Count > 0)
+            {
+                foreach (EventLogEntry item in eventLog1.Entries)
+                {
+                    listView1.Items.Add(item.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("日志中没有记录", "提示");
+            }
+        }
+    }
+}
