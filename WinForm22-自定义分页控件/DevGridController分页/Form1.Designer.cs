a4038a752ef316f4081c890f4bcbeb106db45ebd
shanzm_aimer <2351144690@qq.com>
2022-04-08 13:09:51 +0800

    2022年4月8日 13:09:35-Update：添加Dev中自定义分页控件

---
+++ WinForm22-自定义分页控件/DevGridController分页/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace DevGridController分页
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.mgncPager1 = new WORKALERT.MgncPager();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(12, 12);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(900, 253);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // mgncPager1
+            // 
+            this.mgncPager1.Location = new System.Drawing.Point(12, 262);
+            this.mgncPager1.Name = "mgncPager1";
+            this.mgncPager1.Size = new System.Drawing.Size(900, 33);
+            this.mgncPager1.TabIndex = 1;
+            this.mgncPager1.Load += new System.EventHandler(this.mgncPager1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 307);
+            this.Controls.Add(this.mgncPager1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private WORKALERT.MgncPager mgncPager1;
+    }
+}
+
---
+++ WinForm22-自定义分页控件/DevGridController分页/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WORKALERT;
+
+namespace DevGridController分页
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int curPage = 1;
+        public int pageSize = 100;
+        public int allpage = 1000;
+
+        public void ExportEvents(bool singlePage)     
+        {
+            //导出GridControl代码写在这。
+        }
+        public void RefreshGridList()
+        {
+            FillGridListCtrlQuery(curPage);//自己实现FillGridListCtrlQuery函数。      
+        }
+        private void FillGridListCtrlQuery(int curPage = 1)
+        {
+            gridControl1.DataSource = new DataTable();//WebService.Pager(。。。。。//显示分页结果
+            mgncPager1.RefreshPager(pageSize, allpage, curPage);//更新分页控件显示。
+        }
+        private void MyPagerEvents(int curPage, int pageSize)
+        {
+            this.curPage = curPage;
+            this.pageSize = pageSize;
+            FillGridListCtrlQuery(curPage);
+        }
+        private void mgncPager1_Load(object sender, EventArgs e)
+        {
+            mgncPager1.myPagerEvents += new MgncPager.MyPagerEvents(MyPagerEvents);
+            mgncPager1.exportEvents += new MgncPager.ExportEvents(ExportEvents);
+            RefreshGridList();
+        }
+    }
+}
